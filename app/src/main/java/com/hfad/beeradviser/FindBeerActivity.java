bcf15e1d494271b52b2cfd9b5872c89f7ef054a8
shumvlesu <37502809+shumvlesu@users.noreply.github.com>
2019-01-20 17:25:56 +0300

    страница 100

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -2,6 +2,7 @@ package com.hfad.beeradviser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends Activity {
 
@@ -10,4 +11,10 @@ public class FindBeerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
     }
+
+    private void onClickFindBeer(View view){
+
+    }
+
+
 }
